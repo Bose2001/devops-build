378d745f2abdcded6cc46bf8e3906a2e100d4cfd
EC2 Default User <ec2-user@ip-172-31-40-188.ec2.internal>
2025-08-10 04:59:14 +0000

    Initial commit with Docker setup and scripts

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Build Docker image and tag for dev
+docker build -t bose2001/dev:latest .
+
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Deploy container using docker-compose
+docker-compose up -d
+
